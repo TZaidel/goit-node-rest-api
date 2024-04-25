ef16f8e7964b214889128d974fcaada065e06e2c
Tatiana Zaidel <8tzaidel@gmail.com>
2024-04-25 09:31:35 +0300

    edit contact controller

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -10,26 +10,28 @@ import {catchAsync} from '../helpers/catchAsync.js'
 import HttpError from '../helpers/HttpError.js'
 
 export const getAllContacts = catchAsync(async (req, res) => {
-  const data = await listContacts()
+  const {_id} = req.user
+  const data = await listContacts(_id)
   res.status(200).json(data)
 })
 
 export const getOneContact = catchAsync(async (req, res) => {
   const { id } = req.params
-  const data = await getContactById(id)
+  const data = await getContactById(id, req)
   if (!data) throw HttpError(404, "Not found")
   res.status(200).json(data)
 })
 
 export const deleteContact = catchAsync(async (req, res) => {
   const { id } = req.params
-  const result = await removeContact(id)
+  const result = await removeContact(id, req)
   if (!result) throw HttpError(404, "Not found")
   res.status(200).json(result)
 })
 
 export const createContact = catchAsync(async (req, res) => {
-  const newContact = await addContact(req.body)
+  const {_id: owner} = req.user
+  const newContact = await addContact({...req.body, owner})
   if (!newContact) throw HttpError(400, 'Failed to create contact')
   res.status(201).json(newContact)
 })
@@ -41,7 +43,7 @@ export const updateContact = catchAsync(async (req, res) => {
     throw HttpError(400, "Body is required");
   }
 
-  const result = await updContact(id, req.body)
+  const result = await updContact(req, id, req.body)
   if (!result)  throw HttpError(404, "Not found")
   res.json(result)
 })
@@ -54,7 +56,7 @@ export const updateStatus = catchAsync(async (req, res) => {
     throw HttpError(400, "Body is required");
   }
 
-  const updatedContact = await updStatusContact(id, { favorite })
+  const updatedContact = await updStatusContact(req, id, { favorite })
   if (!updatedContact) throw HttpError(404, "Not found")
   res.status(200).json(updatedContact)
 })
